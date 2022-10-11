306c548065adf97b40c7f7b4794dad9c50996abf
Balasaraswathi-26 <115528028+Balasaraswathi-26@users.noreply.github.com>
2022-10-11 12:00:02 +0530

    Add files via upload
    week1.day1

---
+++ Mobilephone.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobilephone {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mobileBrandName= "Honor";
+System.out.println(mobileBrandName);
+char mobileLogo=   'H';
+System.out.println(mobileLogo);
+short noOfMobilePiece= 26;
+System.out.println(noOfMobilePiece);
+int mobileNumber= 7558;
+System.out.println(mobileNumber);
+long mobileImeiNumber= 987654321L;
+System.out.println(mobileImeiNumber);
+float mobilePrice=45678.98F;
+System.out.println(mobilePrice);
+boolean isDamaged= false;
+System.out.println(isDamaged);
+}
+}
---
+++ Twowheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+	public class Twowheeler {
+		public static void main(String[] args) {
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		 short noOfMirrors=2;
+		 System.out.println(noOfMirrors);
+		 long chassisNumber=56789l;
+		 System.out.println(chassisNumber);
+		 boolean isPunctured = false;
+		 System.out.println(isPunctured);
+		 double bikeKM=7856.98;
+		 System.out.println(bikeKM);
+		 String bikeName= "KTM";
+		 System.out.println(bikeName);
+		 
+			}
+		}
+
+
